3f473d9f0bff0352433553f435200d269caf061c
Albano Díez de Paulino <albanodiezp@gmail.com>
2023-04-02 12:45:23 +0200

    Dar Orden y Esplendor

---
+++ 2ºEvaluacion/Ejercicios adicionales tema 5/src/ejercicios1_09/Ej5.java
@@ -0,0 +1,30 @@
+/*5.- Crear un array con 10 números e invertirlo en el mismo array. Muestra el array
+obtenido*/
+package ejercicios1_09;
+
+
+public class Ej5 {
+
+
+    public static void main(String[] args) {
+        
+        int array[] = new int[]{1,2,3,4,5,6,7,8,9,10,11,12};
+        int cont = array.length-1;
+        int posA;
+        
+        for (int i = 0; i < ((int)array.length/2); i++) {
+            posA =array[i];
+            array[i]=array[cont];
+            array[cont]=posA;
+            cont--;
+        }
+        
+        
+        for (int i : array) {
+            System.out.println(i);
+        }
+    
+    
+    }
+    
+}
---
+++ 2ºEvaluacion/Ejercicios adicionales tema 5/src/ejercicios1_09/Ej8.java
@@ -0,0 +1,27 @@
+/*8.- Realizar una clase que se componga:
+• Un array de cinco elementos de tipo entero.
+• Constructor que solicite al usuario los valores correspondientes a cada elemento.
+• Método que devuelva el valor menor del array.
+• Método que devuelva el valor mayor del array.
+• Método que devuelva la media de todos los valores del array.
+Haz un programa principal que pruebe todos los métodos probados.*/
+package ejercicios1_09;
+
+public class Ej8 {
+
+    public static void main(String[] args) {
+        
+        Ej8Class lista = new Ej8Class();
+        lista.Ej8Class();
+        System.out.println("el menor es "+lista.menor());
+        System.out.println("el mayor es "+lista.mayor());
+        System.out.println("la media es "+lista.media());
+        
+        
+        
+        
+        
+        
+    }
+    
+}
