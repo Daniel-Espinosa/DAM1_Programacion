74588f28dd0a1271f67ebf763c3d96abd91ee059
Daniel-Espinosa <anareldis@gmail.com>
2023-04-11 15:21:48 +0200

    Pruebas para el Reto

---
+++ PracticaDeReto/RetoPruebas/src/ConnectionBBDD.java
@@ -0,0 +1,42 @@
+
+import java.sql.*;
+
+public class ConnectionBBDD {
+
+    static Connection con;
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver"; // esto por lo que entiendo utiliza el driver que bajamos (el jar)
+    static final String USER = "root"; 
+    static final String PASS = "1234";
+    static final String BD = "nba"; // nombre de la base de datos
+    //la direccion para conectar a la bbdd se le pasa como parametro "BD"
+    static final String DB_URL = "jdbc:mysql://localhost:3306/"+BD+"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"; 
+    private static ConnectionBBDD INSTANCE;
+
+    private ConnectionBBDD() {
+
+        try {
+            Class.forName(JDBC_DRIVER).newInstance();
+            con = DriverManager.getConnection(DB_URL, USER, PASS);
+            if (con != null) {
+                System.out.println("coneccion realizada");
+            }
+
+        } catch (SQLException | InstantiationException | IllegalAccessException ex) {
+            System.err.println("problemas de coneccion\n" + ex.toString());
+        } catch (ClassNotFoundException e) {
+            System.err.println(e.toString());
+        }
+    }
+
+    public static ConnectionBBDD getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new ConnectionBBDD();
+        }
+        return INSTANCE;
+    }
+
+    public Connection getConnection() {
+        return con;
+    }
+
+}
---
+++ PracticaDeReto/RetoPruebas/src/NewMain.java
@@ -0,0 +1,13 @@
+
+import java.sql.*;
+
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Connection conexion = ConnectionBBDD.getInstance().getConnection();
+    }
+    
+}
